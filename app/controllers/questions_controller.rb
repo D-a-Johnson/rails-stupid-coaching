0de44f8687b418947651a506301c42db849d415b
Derek Johnson <wars2cool@gmail.com>
2019-03-25 15:21:06 +0900

    Exercise Improved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,16 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+    @coach_message = evaluate(@question)
+  end
+
+  def evaluate(input)
+    if input == 'I am going to work'
+      'Great!'
+    elsif input[-1] == '?'
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
